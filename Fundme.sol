7af6fe5ee29fecc0dc2a3f06c6c812e7887c1adc
maarten <van_den_bergmaarten@hotmail.com>
2023-08-05 17:23:42 +0200

    test added

--- Fundme.sol
+++ Fundme.sol
@@ -23,24 +23,6 @@ contract Fundme {
 
     function withdraw() onlyOwner {}
 
-    /* @dev getting the chainlink pricefeed:*/
-    // we need the pricefeed address for eth/usd sepolia 0x694AA1769357215DE4FAC081bf1f309aDC325306 and/or mocks
-    // and the abi, but... in stead of lokking for the ABI, we're going to us an interface.abi
-    // the AggregatorV3Interface
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        console.log(price); //price of eth in terms of usd. with 8 decimals 1e8
-        //since msg.value is in 1e18 we're doing:
-        return uint256(price * 1e10); // to match decimals
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        // rule for math in solidity is to always multiply before you divide (to keep whole numbers).
-        uint256 ethAmountInUsd = (ethAmountInUsd * ethPrice) / 1e18;
-    }
-
     function getminimumFundAmountUsd() public returns (uint256) {
         return minimumFundAmountUsd;
     }

3b2c4671744f9df26f874490a0fbe0c9126e0ac1
maarten <van_den_bergmaarten@hotmail.com>
2023-08-19 19:38:12 +0200

    test added

--- JS/story-popup.js
+++ JS/story-popup.js
@@ -1,16 +1,16 @@
-const showPopupButton = document.getElementById("storylinePopup"); //temp button for testing
+const showPopupButton = document.getElementById("storylinePopupButton"); //temp button for testing
 const closeStoryPopupButton = document.getElementById("closeStorylinePopup"); //temp button for testing
-const popup = document.getElementById("storylinePopupDiv");
+const popup = document.getElementById("storylinePopupDiv2");
 const popupImage = document.getElementById("storylinePopupImage");
 const popupText = document.getElementById("storylinePopupText");
 
 showPopupButton.addEventListener("click", () => {
   popup.style.right = "0";
   // Replace with your image source and text content
-  popupImage.src = "./images/audio.png";
-  popupText.textContent = "Your popup text goes here.";
+  popupImage.src = "images/Maarten-popup.png";
+  popupText.textContent = "Welcome to Graspop Festival!";
 });
 
 closeStoryPopupButton.addEventListener("click", () => {
-  popup.style.right = "-300px";
+  popup.style.right = "-800px";
 });
